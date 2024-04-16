f4b24f2cd8bf78a9836a07addef2df947f364612
alokankur <71651996+alokankur@users.noreply.github.com>
2024-04-16 23:20:27 +0530

    String variable characters boolean exercise

---
+++ intro.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+//print("myfirst")
+
+//var stringval: String = "lord"
+//print(stringval)
+//let stringval: String = "alok"
+//print(stringval)
+let firststr = "alok"
+let secondstr = "mishra"
+let fullstr = firststr.capitalized + " " + secondstr.capitalized
+print(fullstr)
+//print("fullstr \(fullstr)")
+//print(firststr)
+print(fullstr)
+let name: String="9"
+print(secondstr.capitalized)
+let Name: Int = 9
+print(Name)
+let length = secondstr.count + firststr.count
+print(length)
+print("firststr \(firststr.uppercased())")
+let cap = firststr.uppercased()
+print(cap)
+print("firststr \(secondstr.lowercased())");
+let low = secondstr.uppercased()
+print(low)
+
+
